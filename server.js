8c6493b489c9826f1301fa657dbfa282612dfab1
rozierhj <160989130+rozierhj@users.noreply.github.com>
2024-06-01 10:27:07 -0400

    update routes

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs/promises');
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
-
+//const PORT = 3001;
 app.use(express.static('public'));
 
-console.log('I did stuff');
+app.get('/notes',(req, res)=>{
+    
+    res.sendFile(path.join(__dirname,'/db/db.json'));
+});
+app.post('/notes',(req, res)=>{
+    res.send('you want a piece of me');
+});
+app.delete('/notes/:id',(req,res)=>{
+
+    res.send('ya gone!');
+});
 
 
 app.listen(PORT,()=>{
